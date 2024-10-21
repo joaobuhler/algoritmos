a9258895aa2a2657a6907493766f28eb8fdf5fbc
João Pedro Bühler <joaopedrobuhler26@gmail.com>
2024-10-21 11:41:27 -0300

    crud

---
+++ Comida.js
@@ -0,0 +1,12 @@
+class Comida {
+    constructor(id, nome, peso, distribuidor) {
+        this.id = id;
+        this.nome = nome;
+        this.peso = peso;
+        this.distribuidor = distribuidor;
+      
+
+
+        this.posicaoNaLista = posicaoNaLista; //atributo para facilitar a alteração e exclusão 
+    }
+}
---
+++ ComidaControle.js
@@ -0,0 +1,198 @@
+let listaComida = []; //conjunto de dados
+let oQueEstaFazendo = ''; //variável global de controle
+let comida = null; //variavel global 
+bloquearAtributos(true);
+//backend (não interage com o html)
+function procurePorChavePrimaria(chave) {
+    for (let i = 0; i < listaComida.length; i++) {
+        const comida = listaComida[i];
+        if (comida.id == chave) {
+            comida.posicaoNaLista = i;
+            return listaComida[i];
+        }
+    }
+    return null;//não achou
+}
+
+// Função para procurar um elemento pela chave primária   -------------------------------------------------------------
+function procure() {
+    const id = document.getElementById("inputId").value;
+    if (isNaN(id) || !Number.isInteger(Number(id))) {
+        mostrarAviso("Precisa ser um número inteiro");
+        document.getElementById("inputId").focus();
+        return;
+    }
+
+    if (id) { // se digitou um Id
+        comida = procurePorChavePrimaria(id);
+        if (comida) { //achou na lista
+            mostrarDadosComida(comida);
+            visibilidadeDosBotoes('inline', 'none', 'inline', 'inline', 'none'); // Habilita botões de alterar e excluir
+            mostrarAviso("Achou na lista, pode alterar ou excluir");
+        } else { //não achou na lista
+            limparAtributos();
+            visibilidadeDosBotoes('inline', 'inline', 'none', 'none', 'none');
+            mostrarAviso("Não achou na lista, pode inserir");
+        }
+    } else {
+        document.getElementById("inputId").focus();
+        return;
+    }
+}
+
+//backend->frontend
+function inserir() {
+    bloquearAtributos(false);
+    visibilidadeDosBotoes('none', 'none', 'none', 'none', 'inline'); //visibilidadeDosBotoes(procure,inserir,alterar,excluir,salvar)
+    oQueEstaFazendo = 'inserindo';
+    mostrarAviso("INSERINDO - Digite os atributos e clic o botão salvar");
+    document.getElementById("inputId").focus();
+
+}
+
+// Função para alterar um elemento da lista
+function alterar() {
+
+    // Remove o readonly dos campos
+    bloquearAtributos(false);
+
+    visibilidadeDosBotoes('none', 'none', 'none', 'none', 'inline');
+
+    oQueEstaFazendo = 'alterando';
+    mostrarAviso("ALTERANDO - Digite os atributos e clic o botão salvar");
+}
+
+// Função para excluir um elemento da lista
+function excluir() {
+    bloquearAtributos(false);
+    visibilidadeDosBotoes('none', 'none', 'none', 'none', 'inline'); //visibilidadeDosBotoes(procure,inserir,alterar,excluir,salvar)
+
+    oQueEstaFazendo = 'excluindo';
+    mostrarAviso("EXCLUINDO - clic o botão salvar para confirmar a exclusão");
+}
+
+function salvar() {
+    //gerencia operações inserir, alterar e excluir na lista
+
+// obter os dados a partir do html
+
+    let id;
+    if (comida == null) {
+         id = parseInt(document.getElementById("inputId").value);
+    } else {
+        id = comida.id;
+    }
+
+    const nome = document.getElementById("inputNome").value;
+    const peso = document.getElementById("inputPeso").value;
+    const distribuidor = parseFloat(document.getElementById("inputDistribuidor").value);
+    
+if(id && nome && peso && distribuidor ){// se tudo certo 
+        switch (oQueEstaFazendo) {
+            case 'inserindo':
+                comida = new Comida (id,nome,peso,distribuidor);
+                listaComida.push(comida);
+                mostrarAviso("Inserido na lista");
+                break;
+            case 'alterando':
+                comidaAlterado = new Comida(id,nome,peso,distribuidor);
+                listaComida[comida.posicaoNaLista] = comidaAlterado;
+                mostrarAviso("Alterado");
+                break;
+            case 'excluindo':
+                let novaLista = [];
+                for (let i = 0; i < listaComida.length; i++) {
+                    if (comida.posicaoNaLista != i) {
+                        novaLista.push(listaComida[i]);
+                    }
+                }
+                listaComida = novaLista;
+                mostrarAviso("EXCLUIDO");
+                break;
+            default:
+                // console.error('Ação não reconhecida: ' + oQueEstaFazendo);
+                mostrarAviso("Erro aleatório");
+        }
+        visibilidadeDosBotoes('inline', 'none', 'none', 'none', 'none');
+        limparAtributos();
+        listar();
+        document.getElementById("inputId").focus();
+    } else {
+        alert("Erro nos dados digitados");
+        return;
+    }
+}
+
+//backend
+function preparaListagem(vetor) {
+    let texto = "";
+    for (let i = 0; i < vetor.length; i++) {
+        const linha = vetor[i];
+        texto += 
+            linha.id+" - " +
+            linha.nome+" - " +
+            linha.peso+" - " +
+            linha.distribuidor+"<br>";
+    }
+    return texto;
+}
+
+//backend->frontend (interage com html)
+function listar() {
+    document.getElementById("outputSaida").innerHTML = preparaListagem(listaComida);
+}
+
+function cancelarOperacao() {
+    limparAtributos();
+    bloquearAtributos(true);
+    visibilidadeDosBotoes('inline', 'none', 'none', 'none', 'none');
+    mostrarAviso("Cancelou a operação de edição");
+}
+
+function mostrarAviso(mensagem) {
+    //printa a mensagem na divAviso
+    document.getElementById("divAviso").innerHTML = mensagem;
+}
+
+// Função para mostrar os dados do Filme nos campos
+function mostrarDadosComida(comida) {
+    document.getElementById("inputId").value = comida.id;
+    document.getElementById("inputNome").value = comida.nome;
+    document.getElementById("inputPeso").value = comida.peso;
+    document.getElementById("inputDistribuidor").value = comida.distribuidor;
+ 
+    // Define os campos como readonly
+    bloquearAtributos(true);
+}
+
+// Função para limpar os dados dos campos
+function limparAtributos() {
+    document.getElementById("inputNome").value = "";
+    document.getElementById("inputPeso").value = "";
+    document.getElementById("inputDistribuidor").value = "";
+   
+    bloquearAtributos(true);
+}
+
+function bloquearAtributos(soLeitura) {
+    //quando a chave primaria possibilita edicao, tranca (readonly) os outros e vice-versa
+    document.getElementById("inputId").readOnly = !soLeitura;
+    document.getElementById("inputNome").readOnly = soLeitura;
+    document.getElementById("inputPeso").readOnly = soLeitura;
+    document.getElementById("inputDistribuidor").readOnly = soLeitura;
+}
+
+// Função para deixar visível ou invisível os botões
+function visibilidadeDosBotoes(btProcure, btInserir, btAlterar, btExcluir, btSalvar) {
+    //  visibilidadeDosBotoes('none', 'none', 'none', 'none', 'inline'); 
+    //none significa que o botão ficará invisível (visibilidade == none)
+    //inline significa que o botão ficará visível 
+
+    document.getElementById("btProcure").style.display = btProcure;
+    document.getElementById("btInserir").style.display = btInserir;
+    document.getElementById("btAlterar").style.display = btAlterar;
+    document.getElementById("btExcluir").style.display = btExcluir;
+    document.getElementById("btSalvar").style.display = btSalvar;
+    document.getElementById("btCancelar").style.display = btSalvar; // o cancelar sempre aparece junto com o salvar
+    document.getElementById("inputId").focus();
+}
